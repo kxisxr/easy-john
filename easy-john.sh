762ef85077a2f8b0979f801f3a08c7afbd6b65c0
kxisxr <47679627+kxisxr@users.noreply.github.com>
2025-03-21 01:22:11 -0600

    Update easy-john.sh
    Upgraded

--- easy-john.sh
+++ easy-john.sh
@@ -1,68 +1,80 @@
 #!/bin/bash
-#Author: kxisxr
+# Author: kxisxr
+# Colores
+GREEN="\e[0;32m\033[1m"
+END="\033[0m\e[0m"
+RED="\e[0;31m\033[1m"
+BLUE="\e[0;34m\033[1m"
+YELLOW="\e[0;33m\033[1m"
+PURPLE="\e[0;35m\033[1m"
+TURQUOISE="\e[0;36m\033[1m"
+GRAY="\e[0;37m\033[1m"
 
-greenColour="\e[0;32m\033[1m"
-endColour="\033[0m\e[0m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-purpleColour="\e[0;35m\033[1m"
-turquoiseColour="\e[0;36m\033[1m"
-grayColour="\e[0;37m\033[1m"
+# Solicitar el hash
+echo -e "${YELLOW}Introduce el hash: ${END}"
+read -r hash
 
-echo -e "${yellowColour}"'Put the hash: '"${endColour}"
-read hash
+echo -e '\nAnalizando el hash...\n'
+formats=$(hashid "$hash" | grep -v 'Analyzing' | awk '{print $2}' | tr '-' ' ' | sed 's/ //g' | xargs | tr ' ' '|')
 
-echo -e ' '
-hashid $hash | head -n 4 | grep -v 'Analyzing' 2>/dev/null
-echo -e ' '
-
-echo -e "${yellowColour}"'Probable formats... '"${endColour}"
-var2=$(hashid $hash | head -n 4 | grep -v 'Analyzing' | awk '{print $2}' | tr '-' ' ' | sed 's/\ //g' | xargs | tr ' ' '|')
-john --list=formats | xargs | tr ' ' '\n' | grep -i -E $var2 | tr ',' ' ' | sort -r
-
-echo -e ' '
-echo -e "${yellowColour}"'Put the format: '"${endColour}"
-read format
-echo $hash > $format.hash
-echo -e ' '
-
-echo -e "${yellowColour}"'Path to wordlist: '"${endColour}"
-echo -e ' '
-echo -e "${redColour}"'1.- rockyou.txt '"${endColour}"
-echo -e "${greenColour}"'2.- xato-net-10000.txt '"${endColour}"
-echo -e "${purpleColour}"'3.- top-20-common-SSH-passwords.txt '"${endColour}"
-echo -e "${blueColour}"'4.- md5decryptor-uk.txt '"${endColour}"
-echo -e "${grayColour}"'5.- 000webhost.txt '"${endColour}"
-echo -e ' '
-
-echo -e "${grayColour}"'Select the wordlist...'"${endColour}"
-
-read $wordlist
-
-if [ $wordlist==1 ]
-then
-john --format=$format $format.hash --wordlist=/home/root/Desktop/rockyou.txt >/dev/null 2>&1
-
-elif [ $wordlist==2 ]
-then
-john --format=$format $format.hash --wordlist=/usr/share/wordlists/SecLists/Passwords/xato-net-10-million-passwords-10000.txt >/dev/null 2>&1
-
-elif [ $wordlist==3 ]
-then
-john --format=$format $format.hash --wordlist=/usr/share/wordlists/SecLists/Passwords/Common-Credentials/top-20-common-SSH-passwords.txt >/dev/null 2>&1
-
-elif [ $wordlist==4 ]
-then
-john --format=$format $format.hash --wordlist=/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/md5decryptor-uk.txt >/dev/null 2>&1
-
-elif [ $wordlist==5 ]
-then
-john --format=$format $format.hash --wordlist=/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/000webhost.txt >/dev/null 2>&1
+if [[ -z "$formats" ]]; then
+    echo -e "${RED}No se encontraron formatos probables.${END}"
+    exit 1
 fi
 
-echo -e ' '
-john --show --format=$format $format.hash 2>/dev/null
+# Mostrar formatos probables
+echo -e "${YELLOW}Formatos probables:${END}\n"
+john_formats=$(john --list=formats | tr ' ' '\n' | grep -iE "$formats" | sort -r)
 
+if [[ -z "$john_formats" ]]; then
+    echo -e "${RED}No se encontraron formatos compatibles con John the Ripper.${END}"
+    exit 1
+fi
 
+echo "$john_formats"
+echo -e '\n'
 
+# Solicitar el formato
+echo -e "${YELLOW}Introduce el formato a usar:${END}"
+read -r format
+echo "$hash" > "$format.hash"
+
+echo -e '\n${YELLOW}Selecciona la lista de palabras:${END}\n'
+options=("rockyou.txt" \
+         "xato-net-10000.txt" \
+         "top-20-common-SSH-passwords.txt" \
+         "md5decryptor-uk.txt" \
+         "000webhost.txt")
+
+for i in "${!options[@]}"; do
+    echo -e "${GREEN}$((i+1)).- ${options[$i]}${END}"
+done
+
+echo -e "\n${GRAY}Selecciona una opción (1-5):${END}"
+read -r choice
+
+# Validar la elección
+if ! [[ "$choice" =~ ^[1-5]$ ]]; then
+    echo -e "${RED}Selección no válida.${END}"
+    exit 1
+fi
+
+# Definir rutas de diccionarios
+declare -A wordlists
+wordlists=(
+    [1]="/home/root/Desktop/rockyou.txt"
+    [2]="/usr/share/wordlists/SecLists/Passwords/xato-net-10-million-passwords-10000.txt"
+    [3]="/usr/share/wordlists/SecLists/Passwords/Common-Credentials/top-20-common-SSH-passwords.txt"
+    [4]="/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/md5decryptor-uk.txt"
+    [5]="/usr/share/wordlists/SecLists/Passwords/Leaked-Databases/000webhost.txt"
+)
+
+wordlist=${wordlists[$choice]}
+
+# Ejecutar John
+echo -e "\n${YELLOW}Iniciando ataque con ${wordlist}...${END}"
+john --format="$format" "$format.hash" --wordlist="$wordlist" >/dev/null 2>&1
+
+# Mostrar resultados
+echo -e "\n${YELLOW}Resultados:${END}"
+john --show --format="$format" "$format.hash" 2>/dev/null
